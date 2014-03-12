0265f83fb10e7d5b9870e79bdb108b676902ad20
oliverwreath <oliverwreath@users.noreply.github.com>
2014-03-12 14:05:30 -0700

    Create 8. anti_vowel.py

---
+++ Python/Practise-Makes-Perfect/8. anti_vowel.py
@@ -0,0 +1,8 @@
+def anti_vowel(s):
+    res = "";
+    for c in s:
+        if c == 'A' or c == 'a' or c == 'E' or c == 'e' or c == 'I' or c == 'i' or c == 'O' or c == 'o' or c == 'U' or c == 'u':
+            res
+        else :
+            res += c
+    return res
