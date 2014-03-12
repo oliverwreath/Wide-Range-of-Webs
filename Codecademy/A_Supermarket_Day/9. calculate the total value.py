f3c081746e5793252fe23f04588e10d0b28e82d9
oliverwreath <oliverwreath@users.noreply.github.com>
2014-03-11 23:50:47 -0700

    Create 9. calculate the total value.py

---
+++ Codecademy/A_Supermarket_Day/9. calculate the total value.py
@@ -0,0 +1,19 @@
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+    
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+
+count = 0 
+for item in stock:
+    count = count + prices[item] * stock[item];
+    print count
