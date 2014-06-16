32636b33c22452b5db91646bdd6e763c2e7a4bf6
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 15:58:46 +0800

    Update requestHandlers.js

--- Node.js/requestHandlers.js
+++ Node.js/requestHandlers.js
@@ -4,21 +4,21 @@ function start(res, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
+	'<head>'+
+	'<meta http-equiv="Content-Type" content="text/html; '+
+	'charset=UTF-8" />'+
+	'</head>'+
+	'<body>'+
+	'<form action="/upload" method="post">'+
+	'<textarea name="text" rows="20" cols="60"></textarea>'+
+	'<input type="submit" value="Submit text" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
 
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(body);
-    res.end();
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(body);
+	res.end();
 }
 
 function upload(res, postData){
