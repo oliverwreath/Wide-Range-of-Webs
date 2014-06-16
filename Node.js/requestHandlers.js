e3742d812ac39b6e903931d04c11bce1c7964635
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 15:22:42 +0800

    Update requestHandlers.js

--- Node.js/requestHandlers.js
+++ Node.js/requestHandlers.js
@@ -1,18 +1,22 @@
-function start(){
+var exec = require("child_process").exec;
+
+function start(res){
 	console.log("Request handler 'start' was called.");
 
-	function sleep(milliSeconds){
-		var startTime = new Date().getTime();
-		while( new Date().getTime() < startTime + milliSeconds );
-	}
-
-	sleep(10000);
-	return "Hello Start";
+	exec("find /",
+		{ timeout: 10000, maxBuffer: 20000*1024 },
+		function (error, stdout, stderr) {
+			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.write(stdout);
+			res.end();
+		});
 }
 
-function upload(){
+function upload(res){
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write("Hello Upload");
+	res.end();
 }
 
 exports.start = start;
