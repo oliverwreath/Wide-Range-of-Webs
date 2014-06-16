492c8ff6a5e7d46a0abda0e44c03be332dfeaed3
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 15:58:17 +0800

    Update requestHandlers.js

--- Node.js/requestHandlers.js
+++ Node.js/requestHandlers.js
@@ -1,21 +1,30 @@
-var exec = require("child_process").exec;
+querystring = require('querystring');
 
-function start(res){
-	console.log("Request handler 'start' was called.");
+function start(res, postData) {
+  console.log("Request handler 'start' was called.");
 
-	exec("find /",
-		{ timeout: 10000, maxBuffer: 20000*1024 },
-		function (error, stdout, stderr) {
-			res.writeHead(200, {"Content-Type": "text/plain"});
-			res.write(stdout);
-			res.end();
-		});
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
 }
 
-function upload(res){
+function upload(res, postData){
 	console.log("Request handler 'upload' was called.");
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write("Hello Upload");
+	res.write("You've sent: " + querystring.parse(postData).text);
 	res.end();
 }
 
