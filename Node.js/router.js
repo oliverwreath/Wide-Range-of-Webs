7fb6712791571066f0e3abb556aa1f56f2517d59
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 15:22:20 +0800

    Update router.js

--- Node.js/router.js
+++ Node.js/router.js
@@ -1,12 +1,13 @@
-function route(handle, pathname){
+function route(handle, pathname, res){
 	console.log('About to route a request for ' + pathname);
 	if(typeof handle[pathname] === 'function'){
-		handle[pathname]();
+		handle[pathname](res);
 	}else{
 		console.log("No request handler found for " + pathname);
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.write("404 Not Found");
+		res.end();
 	}
 }
-
-
 
 exports.route = route; 
