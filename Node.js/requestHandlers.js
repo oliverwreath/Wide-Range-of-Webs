bd944805f7200534f473bac116ee948ea6053e5f
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 18:20:20 +0800

    Update requestHandlers.js

--- Node.js/requestHandlers.js
+++ Node.js/requestHandlers.js
@@ -1,34 +1,61 @@
-querystring = require('querystring');
+var querystring = require('querystring'),
+	fs = require('fs'),
+	formidable = require('formidable');
 
-function start(res, postData) {
-  console.log("Request handler 'start' was called.");
+function start(res) {
+	console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-	'<head>'+
-	'<meta http-equiv="Content-Type" content="text/html; '+
-	'charset=UTF-8" />'+
-	'</head>'+
-	'<body>'+
-	'<form action="/upload" method="post">'+
-	'<textarea name="text" rows="20" cols="60"></textarea>'+
-	'<input type="submit" value="Submit text" />'+
-	'</form>'+
-	'</body>'+
-	'</html>';
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" enctype="multipart/form-data" '+
+		'method="post">'+
+		'<input type="file" name="upload" multiple="multiple">'+
+		'<input type="submit" value="Upload file" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
 
 	res.writeHead(200, {"Content-Type": "text/html"});
 	res.write(body);
 	res.end();
 }
 
-function upload(res, postData){
-	console.log("Request handler 'upload' was called.");
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write("You've sent: " + querystring.parse(postData).text);
-	res.end();
+function upload(res, req){
+	console.log("Request handle 'upload' was called");
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(req, function(error, fields, files){
+		console.log("parsing done");
+		fs.renameSync(files.upload.path, "/tmp/test.png");
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write("received image:<br/>");
+		res.write("<img src='/show' />");
+		res.end();
+	});
+}
+
+function show(res){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/tmp/test.png", "binary", function(error, file){
+		if( error ){
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.write(error + "\n");
+			res.end();
+		}else{
+			res.writeHead(200, {'Content-Type': 'image/png'});
+			res.write(file, "binary");
+			res.end();
+		}
+	})
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
 
