93064ed00d3246d7255e7cd6d642f23ace214ed0
oliverwreath <oliverwreath@users.noreply.github.com>
2014-06-16 14:17:09 +0800

    Update server.js

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-function start(route){
+function start(route, handle){
 	function onRequest(req, res){
 		var pathName = url.parse(req.url).pathname;
 		console.log('Request for ' + pathName + ' received');
 
-		route(pathName);
+		route(handle, pathName);
 
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.write('Hello World');
